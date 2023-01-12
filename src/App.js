15a5cf7e74f2e606348cd6321cf21900261e2ee2
David Fischer <david.fischer@newulife.com>
2023-01-12 11:54:02 -0700

    abc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./app.css";
-import LoginPage from "./components/pages/login";
+// import LoginPage from "./components/pages/login";
+import firstPro from "./configs/firstpro.json";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <Header />
       <Content>
         <div>content</div>
-        <LoginPage />
+        {/* <LoginPage /> */}
       </Content>
       <Footer />
     </>
@@ -17,39 +18,62 @@ function App() {
 export default App;
 
 function Header(props) {
+  const { name, sections, logo } = firstPro;
   const { children } = props;
+  if (!sections.includes("header")) return null;
   return (
     <header>
-      <div>header</div>
+      <img src={logo} alt="logo" />
+      <span>{name}</span>
+      <Menu />
       {children}
     </header>
   );
 }
 function Aside(props) {
   const { children } = props;
+  const { sections } = firstPro;
+  if (!sections.includes("aside")) return null;
   return (
     <aside>
       <div>aside</div>
+      <Nav />
       {children}
     </aside>
   );
 }
 function Content(props) {
   const { children } = props;
-  const showAside = true;
   return (
     <div className="content region">
-      {showAside && <Aside />}
+      <Aside />
       <div className="section content">{children}</div>
     </div>
   );
 }
 function Footer(props) {
   const { children } = props;
+  const { sections } = firstPro;
+  if (!sections.includes("header")) return null;
   return (
     <footer>
       <div>footer</div>
       {children}
     </footer>
   );
+}
+function Menu(props) {
+  return <span tabIndex="0">MENU</span>;
+}
+function Nav(props) {
+  const navList = [
+    "navItem 1",
+    "navItem 2",
+    "navItem 3",
+    "navItem 4",
+    "navItem 5",
+  ];
+  return navList.map((item) => {
+    return <span>{item}</span>;
+  });
 }
